706a3e40e3f5c79d43aa8025fbf7235da95881a7
frosty <inthishouseofcards@gmail.com>
2024-01-22 19:37:49 -0500

    fixed some stuff, now using pyproject

--- README.md
+++ README.md
@@ -1,49 +1,24 @@
 # rmempty
 
-rmempty is a tool to remove all empty directories from a directory tree. It's a simple command to call from the terminal, and should also work when imported from other Python modules.
-
-```
-$ rmempty "D:\rmempty"
-Removed: D:\rmempty\folderup\alittlelower\really\woahh
-Removed: D:\rmempty\folderup\alittlelower\really
-Removed: D:\rmempty\folderup\alittlelower
-Removed: D:\rmempty\folderup
-Removed: D:\rmempty\overhere\hahayeah\itsreal
-Removed: D:\rmempty\overhere\hahayeah
-Removed: D:\rmempty\overhere\whoops
-Removed: D:\rmempty\overhere
-Removed: D:\rmempty
-```
-
-**Note:** in the snippet provided, it deleted the directory I gave it. This is because it was empty after deleting all the empty folders inside it. rmempty will not remove any folders if there's content inside.
-
-## Dependencies
-
-- Python (`>=3.11`) with `click` installed
+Removes empty directories from a directory tree.
 
 ## Usage
-
-rmempty uses [Click](https://click.palletsprojects.com/en/) for the command line interface. The text snippet is taken from the help menu accessed via `rmempty --help`.
 
 ```
 Usage: rmempty [OPTIONS] [PATH]
 
-  Remove empty directories from a directory tree.
+  Removes empty directories from a directory tree.
 
   By default, it will use the current directory if no PATH is given.
 
-  Copyright (c) 2023 frosty.
+  Copyright (c) 2024 frosty.
 
 Options:
   -h, --help  Show this message and exit.
-
-  Source code: https://github.com/st0rmw1ndz/rmempty
 ```
 
 ## Installation
 
-Installation is a simple `pip` command using the git repo as the URL. It should be able to automatically download the dependencies.
-
 ```
-pip install git+https://github.com/st0rmw1ndz/rmempty
+pip install git+https://github.com/st0rmw1ndz/rmempty.git
 ```
--- rmempty/__main__.py
+++ rmempty/__main__.py
@@ -1,48 +1,33 @@
-import os
+from pathlib import Path
 
 import click
 
-import rmempty
 
-HELP: str = """Remove empty directories from a directory tree.
+def rmempty(directory: Path) -> None:
+    if not any(directory.iterdir()):
+        directory.rmdir()
+        click.echo(f"Removed '{directory}'")
+        if directory.parent != Path():
+            rmempty(directory.parent)
 
-By default, it will use the current directory if no PATH is given.
 
-Copyright (c) 2023 frosty.
-"""
-
-
-@click.command(
-    context_settings=dict(help_option_names=["-h", "--help"]),
-    name="rmempty",
-    help=HELP,
-    epilog="Source code: https://github.com/st0rmw1ndz/rmempty",
-)
+@click.command(context_settings=dict(help_option_names=["-h", "--help"]))
 @click.argument(
     "path",
     nargs=1,
     default=".",
     type=click.Path(exists=True, file_okay=False, dir_okay=True),
 )
-@click.version_option(rmempty.__version__, "-v", "--version")
-def rmempty(path: str) -> None:
+def cli(path: str) -> None:
     """Removes empty directories from a directory tree.
-    :param path: Directory path.
-    :return: None.
+
+    By default, it will use the current directory if no PATH is given.
+
+    Copyright (c) 2024 frosty.
     """
-    for root, dirs, files in os.walk(path, topdown=False):
-        if not dirs and not files:
-            os.rmdir(root)
-            if __name__ == "__main__":
-                click.echo(f"Removed: {root}")
-            parent = os.path.dirname(root)
-            while parent and parent != ".":
-                if not os.listdir(parent):
-                    os.rmdir(parent)
-                    if __name__ == "__main__":
-                        click.echo(f"Removed: {parent}")
-                parent = os.path.dirname(parent)
+    for directory in list(Path(path).rglob("")):
+        rmempty(directory)
 
 
 if __name__ == "__main__":
-    rmempty()
+    cli()
